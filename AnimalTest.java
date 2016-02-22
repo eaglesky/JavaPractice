31745e567c9d31430a4b58c6d467c5440e21ec6f
eaglesky <allenchin1990@gmail.com>
2016-02-21 22:52:09 -0800

    Add one line to AnimalTest to test overriden method

--- AnimalTest.java
+++ AnimalTest.java
@@ -4,5 +4,6 @@ public class AnimalTest {
 		Animal animal2 = new Cat(1, 2, 3);
 		System.out.println("Animal 1 is " + animal1.getClass());
 		System.out.println("Animal 2 is " + animal2.getClass());
+		System.out.println(animal2);
 	}
 }
