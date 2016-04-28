277fe7177835947efc066bdf2095d2a17c725ff5
Yalun Qin <yqin@rocketfuel.com>
2016-04-27 23:31:56 -0700

    Add ToUpperCaseTest.java

---
+++ ToUpperCaseTest.java
@@ -0,0 +1,8 @@
+public class ToUpperCaseTest {
+	public static void main(String[] args) {
+		String testStr = "asdf22";
+		String result = testStr.toUpperCase();
+		System.out.println("Original string = " + testStr);
+		System.out.println("Converted string = " + result);
+	}
+}
