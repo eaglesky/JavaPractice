8e1d48e4d2de31d181bc8708403f20924c90ce67
Yalun Qin <yqin@rocketfuel.com>
2016-12-25 22:11:37 +0800

    Add TestBitwiseOperations.java

---
+++ TestBitwiseOperations.java
@@ -0,0 +1,14 @@
+public class TestBitwiseOperations {
+	public static void main(String[] args) {
+		int num = (1 << 30);
+		System.out.println("num = " + num);
+		System.out.println("After arithmetic shifting left by 1: " + (num << 1));
+		System.out.println("num = " + (num));
+		System.out.println((num << 1) - 1);
+		System.out.println(Integer.toBinaryString((num << 1) - 1));
+		System.out.println(Integer.toBinaryString(-1));
+		//There is no '<<<'
+
+		System.out.println(Integer.SIZE);
+	}
+}
